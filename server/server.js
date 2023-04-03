8a8f460d38f3dfb21c43cd2cd498018d3c79c53b
120693 <dolphinn9302@gmail.com>
2023-04-03 13:06:57 +0900

    mongoDB connect

--- server/server.js
+++ server/server.js
@@ -7,3 +7,9 @@ app.use("/api", test);
 const port = 5000; // 노드 서버가 사용할 포트 넘버
 
 app.listen(port, () => console.log(`${port}`));
+
+// MongoDB 연결
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://HHm:HHm230327@hhm.8k6d6kh.mongodb.net/test')
+    .then(() => console.log("mongoDB connected...")) // 연결 성공
+    .catch(err => console.log(err)) // 연결 err
